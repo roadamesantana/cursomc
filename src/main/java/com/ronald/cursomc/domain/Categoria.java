8ee91d8ff0f3a63a17f63094b1abdc1f8d478ecf
Ronald Santana <roadamesantana@gmail.com>
2019-05-07 09:47:33 +0100

    Banco de dados H2 - MVN dependencias.

--- src/main/java/com/ronald/cursomc/domain/Categoria.java
+++ src/main/java/com/ronald/cursomc/domain/Categoria.java
@@ -2,9 +2,17 @@ package com.ronald.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
@@ -31,6 +39,11 @@ public class Categoria implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	@Override
+	public String toString() {
+		return "ID: " + getId() + " | Nome: " + getNome();
 	}
 
 	@Override
