7ef1ba9e9660f794b59469e18ee044e535f66455
Ronald Santana <roadamesantana@gmail.com>
2019-05-07 21:16:56 +0100

    m

--- src/main/java/com/ronald/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ronald/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,6 @@
 package com.ronald.cursomc.resources;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,14 +14,10 @@ import com.ronald.cursomc.domain.Categoria;
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-    public List<Categoria> listar(){
+	public List<Categoria> listar() {
 		Categoria cat1 = new Categoria(1, "Informática");
 		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);		
-		
-        return lista;
-    }
+
+		return Arrays.asList(cat1, cat2);
+	}
 }
