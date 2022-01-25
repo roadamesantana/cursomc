f2f9520782470f65e0e92c364e4d6df6e871b63a
Ronald SantAna <roadamesantana@gmail.com>
2022-01-25 17:42:10 +0000

    Ajustes no End Point /categorias/{id}

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -4,19 +4,21 @@ import java.util.Optional;
 
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoriaService {
-    
+
     @Autowired
     private CategoriaRepository repo;
 
     public Categoria buscar(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 
 }
